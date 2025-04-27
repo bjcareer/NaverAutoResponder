35af886f41f9b1244b626872528be8acd91b27d6
baejaewan <bjwan.career@gmail.com>
2025-04-27 19:05:00 +0900

    [feat] Add login service - 네이버 로그인을 위한 기능 추가
    related to #1

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 import {ChromDriver} from "./chrom/utils/chromDriver";
+import {LoginService} from "./naver/service/loginService";
+import {LoginCommand} from "./naver/in/loginCommand";
 
 async function main(): Promise<void> {
     const chromDriver = new ChromDriver();
+    const loginService = new LoginService();
     const driver = await chromDriver.createDriver();
 
     try {
         // 테스트할 시나리오
         await driver.get('https://www.naver.com/');
         console.log(await driver.getTitle());
+        await driver.sleep(5000);
+        await loginService.login(new LoginCommand('changeme', 'changeme'), driver);
+
+        // driver.wait(10000);
+
     } finally {
         await driver.quit();
     }
